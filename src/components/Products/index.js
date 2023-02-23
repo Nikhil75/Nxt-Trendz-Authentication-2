b27055e18445c90951533808dddd1f25987d20a5
Nikhil75 <nikhilmengani0707@gmail.com>
2023-02-23 08:53:10 +0000

    Nxt Trendz Authentication 2

---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="cart-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="cart"
+      />
+    </div>
+  </>
+)
+
+export default Products
